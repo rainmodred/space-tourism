d58c25cffae84a7a7a4fd9489225634741420752
rainmodred <albphoenix@gmail.com>
2021-12-13 20:49:57 +0300

    Fix Tab focus

--- components/Tabs/Tab/Tab.js
+++ components/Tabs/Tab/Tab.js
@@ -1,10 +1,19 @@
+import { useEffect, useRef } from 'react';
 import { makeId, useTabs } from '../useTabs';
 import styles from './Tab.module.css';
 
 export function Tab({ children, tabIndex }) {
   const { selectedTabIndex, onSelect } = useTabs();
 
+  const ref = useRef(null);
+
   const isSelected = tabIndex === selectedTabIndex;
+
+  useEffect(() => {
+    if (tabIndex === selectedTabIndex) {
+      ref.current.focus();
+    }
+  }, [selectedTabIndex, tabIndex]);
 
   return (
     <button
@@ -16,6 +25,7 @@ export function Tab({ children, tabIndex }) {
       aria-controls={makeId('tab', tabIndex)}
       tabIndex={isSelected ? 0 : -1}
       className={`${isSelected ? styles.selected : ''}`}
+      ref={ref}
     >
       {children}
     </button>
